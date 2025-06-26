00849daeec8e76e5030794f0239896950ff6b204
PedroNeumann <phnrezende08@gmail.com>
2025-06-26 01:30:57 -0300

    Segunda Tela de preferências de usuário pronto

--- app/src/main/java/com/pedro/trabalho/MainActivity.kt
+++ app/src/main/java/com/pedro/trabalho/MainActivity.kt
@@ -11,11 +11,14 @@ import com.pedro.trabalho.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-    }
-}
+        binding.continuar.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+
+        }
+}}
