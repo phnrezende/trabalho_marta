97683b6cbb6a4041e00619b761147d720820e59a
Chutulio <tulio.2003@hotmail.com>
2025-10-10 09:44:41 -0300

    notificacoes

--- app/src/main/java/com/pedro/trabalho/MenuActivity.kt
+++ app/src/main/java/com/pedro/trabalho/MenuActivity.kt
@@ -2,6 +2,8 @@ package com.pedro.trabalho
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 import com.pedro.trabalho.databinding.ActivityMenuBinding
 
@@ -13,8 +15,38 @@ class MenuActivity : AppCompatActivity() {
         binding = ActivityMenuBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // Clique no botão "Química"
         binding.quimica.setOnClickListener {
             val intent = Intent(this, QuimicaActivity::class.java)
+            startActivity(intent)
+        }
+
+        // Referência à navbar incluída no layout
+        val navBar = findViewById<LinearLayout>(R.id.navBar)
+
+        // Botão HOME → Voltar para MenuActivity
+        navBar.findViewById<ImageView>(R.id.nav_home).setOnClickListener {
+            val intent = Intent(this, MenuActivity::class.java)
+            // Evita abrir múltiplas instâncias da mesma tela
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP
+            startActivity(intent)
+        }
+
+        // Botão SIMULADOS → Abrir SimuladosActivity
+        navBar.findViewById<ImageView>(R.id.nav_simulados).setOnClickListener {
+            val intent = Intent(this, SimuladosFragment::class.java)
+            startActivity(intent)
+        }
+
+        // Botão NOTIFICAÇÕES → Abrir NotificacoesActivity
+        navBar.findViewById<ImageView>(R.id.nav_notificacoes).setOnClickListener {
+            val intent = Intent(this, NotificacoesFragment::class.java)
+            startActivity(intent)
+        }
+
+        // Botão PERFIL → Abrir PerfilActivity
+        navBar.findViewById<ImageView>(R.id.nav_perfil).setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
         }
     }
